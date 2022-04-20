db1a3832414bf3b83363647adf0124bf9b94b5c4
Estebab Cueto <encuetor@gmail.com>
2022-04-20 16:20:37 -0400

    Taller 3 | Promedio, Media y Moda Listo

--- README.md
+++ README.md
@@ -21,3 +21,13 @@
 - Tercer paso: Crear funciones
 
 - Cuarto paso: integrar Js en HTML
+
+## Taller #3: Promedio, media y moda
+
+- Primer paso: definir las formulas
+
+- Segundo paso: Implementar las formulas en JavaScript
+
+- Tercer paso: Crear funciones
+
+- Cuarto paso: integrar Js en HTML
---
+++ moda.js
@@ -0,0 +1,34 @@
+const lista1 = [
+    1,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    2,
+    1
+]
+
+const lista1Count = {}
+
+lista1.map(
+    function (elemento){
+        if(lista1Count[elemento]){
+            lista1Count[elemento] += 1;
+        }else{
+            lista1Count[elemento] = 1
+        }
+        
+    }
+)
+
+const lista1Array = Object.entries(lista1Count).sort(
+    function(elemento1, elemento2){
+       return elemento1[1] - elemento2[1]
+    }
+)
+
+const moda = lista1Array[lista1Array.length - 1]
